2d7a5d4186285854e12c0e2ec83c27b118765536
gregrecco67 <127459177+gregrecco67@users.noreply.github.com>
2024-02-10 14:19:30 -0500

    license notice

--- Source/MoonKnob.cpp
+++ Source/MoonKnob.cpp
@@ -1,3 +1,18 @@
+/*
+ * Audible Planets - an expressive, quasi-Ptolemaic semi-modular synthesizer
+ *
+ * Copyright 2024, Greg Recco
+ *
+ * Audible Planets is released under the GNU General Public Licence v3
+ * or later (GPL-3.0-or-later). The license is found in the "LICENSE"
+ * file in the root of this repository, or at
+ * https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * All source for Audible Planets is available at
+ * https://github.com/gregrecco67/AudiblePlanets
+ */
+
+
 #include "MoonKnob.h"
 //==============================================================================
 MoonKnob::MoonKnob(gin::Parameter* p, bool fromCentre)
